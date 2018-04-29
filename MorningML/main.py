cab4fedebda9c6a81d7d9e8166c1a7667c6d7f7f
Ahmad Hassan <ahmadhassanch@hotmail.com>
2018-04-29 20:19:06 +0500

    .

--- MorningML/main.py
+++ MorningML/main.py
@@ -5,7 +5,16 @@ from NNInput import NNInput
 from NNInnerProduct import NNInnerProduct
 from NNRelu import NNRelu
 from NNSigmoid import NNSigmoid
-import scipy
+import scipy.io as sio
+
+
+def genData():
+	refX = (np.random.rand(2,m)*10).round();
+	refY = (np.random.rand(1,m)*1).round();
+	refDataX.data = refX;
+	refDataY.data = refY;
+	sio.savemat("refData", {'refX':refX, 'refY':refY})
+
 
 def main():
 	m = 3;
@@ -13,13 +22,10 @@ def main():
 	refDataX = NNData(2, m);
 	refDataY = NNData(1, m);
 	
-	refX = (np.random.rand(2,m)*10).round();
-	refY = (np.random.rand(1,m)*1).round();
-
-	scipy.io.savemat("refData", {refX:refX, refY, refY})
-
-	refDataX.data = refX;
-	refDataX.mPrint()
+	genData(refDataX, refDataY)	
+		
+	refDataY.mPrint()
+	exit()
 	
 	net    = NNetwork();
 	layer0 = NNInput(net, 'Input0', 2);
